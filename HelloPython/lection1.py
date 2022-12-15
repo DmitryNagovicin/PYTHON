f6f29e9c1796fd1edd1001f3f6e570c79bb2b045
Dmitry Nagovitsyn <nagovicinde@mail.ru>
2022-12-15 21:35:57 +0400

    HelloPython folder with lections added

---
+++ HelloPython/lection1.py
@@ -0,0 +1,48 @@
+#print("hello world")
+
+
+#value = None
+# print(type(value))
+#a = 123
+#b = 1.23
+# print(a)
+# print(b)
+
+#value = 12334
+
+# print(type(a))
+# print(type(b))
+# print(type(value))
+
+
+#s = 'hello'
+# print(s)  # вывод строковой переменной
+
+#print(a, '-', b, '-', s)
+
+#print('{} - {} - {}'.format(a, b, s))
+
+#print(f'{a} - {b} - {s}')
+
+#f = True
+# print(f)
+
+# list = ['1', '2', '3']
+# col = ['hello', 'world']
+# print(list)
+# print(col)
+
+
+# print('Введите a')
+# a = int(input())
+# print('Введите b')
+# b = int(input())
+# print(a, '+', b, '=' , a + b)
+# print('{},{}'.format(a, b))
+# print(f'{a},{b}')
+
+
+a = 12
+b = 10
+c = a+b
+print(c)
